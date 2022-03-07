b9579e2766ae8314f6ece56b56f831a53a400e12
NikitaCherskov <nikitacherskov@gmail.com>
2022-03-07 13:27:19 +0300

    Сделал минимальный калькулятор

--- calculate_function/calculate_function.cpp
+++ calculate_function/calculate_function.cpp
@@ -1,20 +1,109 @@
-﻿// calculate_function.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <locale>
 
-#include <iostream>
+float postcalc(char* example, int size)
+{
+    int i, j;
+    int level = 0; //компилятор ругается без = 0
+    for (i = 0, level = 0; i < size; i++)
+    {
+        if (example[i] == '(') //оптимизировать
+        {
+            level++;
+        }
+        else if (example[i] == ')')
+        {
+            level--;
+        }
+        if (example[i] == '+' && level == 0)
+        {
+            return postcalc(example, i) + postcalc(&(example[i + 1]), size - (i + 1));
+        }
+        if (example[i] == '-' && level == 0)
+        {
+            return postcalc(example, i) - postcalc(&(example[i + 1]), size - (i + 1));
+        }
+    }
+    for (i = size - 1, level = 0; i > -1; i--)
+    {
+        if (example[i] == ')') //оптимизировать
+        {
+            level++;
+        }
+        else if (example[i] == '(')
+        {
+            level--;
+        }
+        if (example[i] == '*' && level == 0)
+        {
+            return postcalc(example, i) * postcalc(&(example[i + 1]), size - (i + 1));
+        }
+        if (example[i] == '/' && level == 0)
+        {
+            return postcalc(example, i) / postcalc(&(example[i + 1]), size - (i + 1));
+        }
+    }
+    for (i = 0, level = -1; i < size; i++)
+    {
+        if (example[i] == '(') //оптимизировать
+        {
+            for (j = 0; j < size; j++)
+            {
+                if (example[j] == '(')
+                {
+                    level++;
+                }
+                else if (example[j] == ')' && level != 0)
+                {
+                    level--;
+                }
+                else if (example[j] == ')' && level == 0)
+                {
+                    return postcalc(&(example[i + 1]), j - (i + 1));
+                }
+            }
+        }
+    }
+    char* float_str = new char[size + 1];
+    for (i = 0; i < size; i++)
+    {
+        float_str[i] = example[i];
+    }
+    float_str[i] = '\0';
+    return atof(float_str);
+}
+float calculate(const char* example)
+{
+    int i, j;
+    int size;
+    float answer;
+    char* exam;
+    for (i = 0, size = 0; example[i] != '\0'; i++)
+    {
+        if (example[i] != ' ')
+        {
+            size++;
+        }
+    }
+    exam = new char[size];
+    for (i = 0, j = 0; example[i] != '\0'; i++)
+    {
+        if (example[i] != ' ')
+        {
+            exam[j] = example[i];
+            j++;
+        }
+    }
+    answer = postcalc(exam, size);
+    delete[] exam;
+    return answer;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(0, "");
+    char* f = new char[100];
+    std::cout << "введите форулу:\n";
+    std::cin >> f;
+    std::cout << "ответ: " << calculate(f);
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
